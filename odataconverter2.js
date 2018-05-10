80ae9d6ee007c0d9ab5e28a5b25358557bdcbe4b
James Magin <james@cothamtechnologies.com>
2018-05-10 17:58:24 +0100

    posapmetat

--- odataconverter2.js
+++ odataconverter2.js
@@ -2,7 +2,7 @@ const { odata2openapi } = require('odata2openapi');
 
 const fs = require('fs');
 
-odata2openapi('https://raw.githubusercontent.com/mjmagin/openAPI-Files/master/sapES5GWSampleBasicMetadata.xml')
+odata2openapi('https://raw.githubusercontent.com/mjmagin/openAPI-Files/master/te-metadata.xml')
   .then(swagger => console.log(JSON.stringify(swagger, null, 2)))
 
   .catch(error => console.error(error))
